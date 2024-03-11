5b46de4a06160c3d3556724cff7f19272de6b766
Abdur Razzaque <aabdurrazzaque72@gmail.com>
2024-03-11 14:33:46 +0530

    Modified

--- My Portfolio/script.js
+++ My Portfolio/script.js
@@ -1,5 +1,21 @@
+let a = 0
 let button = document.getElementById("toggle");
-let box = document.querySelectorAll(".box");
-button.onclick = function() {
-    box.style.visibility = "hidden";
+let left_blank_boxes = document.querySelectorAll(".translate-left");
+let right_blank_boxes = document.querySelectorAll(".translate-right");
+button.onclick = function () {
+    if (!a) {
+        a++;
+    }
+    else {
+        a--;
+    }
+
+    left_blank_boxes.forEach((blank_box) => {
+        blank_box.style.transform = blank_box.style.transform == "rotate(90deg)" ? "rotate(0deg)" : "rotate(90deg)";
+    })
+
+    right_blank_boxes.forEach((blank_box) => {
+        blank_box.style.transform = blank_box.style.transform == "rotate(90deg)" ? "rotate(0deg)" : "rotate(90deg)";
+    })
+
 }
