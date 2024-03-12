1ce9fefd63e0998dfbdb322c0c3b2bd3ef388dda
Abdur Razzaque <aabdurrazzaque72@gmail.com>
2024-03-13 01:37:45 +0530

    Modified

--- My Portfolio/script.js
+++ My Portfolio/script.js
@@ -1,6 +1,7 @@
 let a = 0
 let button = document.getElementById("toggle");
 let left_blank_boxes = document.querySelectorAll(".translate-left");
+let audio = document.getElementById("audio");
 let right_blank_boxes = document.querySelectorAll(".translate-right");
 button.onclick = function () {
     if (!a) {
@@ -12,6 +13,7 @@ button.onclick = function () {
 
     left_blank_boxes.forEach((blank_box) => {
         blank_box.style.transform = blank_box.style.transform == "rotate(90deg)" ? "rotate(0deg)" : "rotate(90deg)";
+        audio.play();
     })
 
     right_blank_boxes.forEach((blank_box) => {
